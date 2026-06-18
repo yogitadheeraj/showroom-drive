d82c420c73eb2ebdcd910f24840ffa629709f4ff
My Apps <69039185+yogitadheeraj@users.noreply.github.com>
2026-06-18 23:13:51 +0400

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
@@ -15,7 +14,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "images/auth_logo.png", "images/autoadvant-logo.png", "robots.txt"],
