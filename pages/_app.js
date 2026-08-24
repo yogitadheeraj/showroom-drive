3f4aff60430f96265554490058caffaca3f046f6
Peers Plus <peersplushr@gmail.com>
2026-08-25 00:32:54 +0400

    ADDED SCRIPT

--- pages/_app.js
+++ pages/_app.js
@@ -177,6 +177,18 @@ export default function App({ Component, pageProps }) {
         }}
       />
       {/* End Google Tag Manager */}
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QSBYHHR3H1"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QSBYHHR3H1');
+          `,
+        }}
+      />
       </Head>
       <ThemeProvider>
         <WhitelabelProvider>
